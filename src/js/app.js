b5e7470860a1a1b8fda208f4dd370d93d4f8e214
Shahar <shahar0897@gmail.com>
2018-12-14 21:54:56 +0200

    coloring a function is now working

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,22 @@
 import $ from 'jquery';
 import {parseCode} from './code-analyzer';
-
+let input_counter = 0;
 $(document).ready(function () {
     $('#codeSubmissionButton').click(() => {
         let codeToParse = $('#codePlaceholder').val();
-        let parsedCode = parseCode(codeToParse);
-        $('#parsedCode').val(parsedCode);
+
+        let inputs = [];
+        for(let i = 0; i < input_counter; i++){
+            inputs.push(document.getElementById('input_'+i).value);
+        }
+        let coloredCode = parseCode(codeToParse,inputs);
+        document.getElementById('parsedCode').innerHTML = coloredCode;
+    });
+
+    $('#add-input-button').click(()=>{
+        let element = document.createElement('input');
+        element.type = 'text';
+        element.id = 'input_'+input_counter++;
+        document.getElementById('input_div').appendChild(element);
     });
 });
--- src/js/code-analyzer.js
+++ src/js/code-analyzer.js
@@ -1,8 +1,9 @@
 import * as esprima from 'esprima';
+import * as escodegen from 'escodegen';
+
 let beautify = require('js-beautify');
 
-
-let legalCharacters = ['+', '-', '/', '*', '<', '>', '=', '&', '|', '(', ')', ';', ',', '^'];
+let legalCharacters = ['+', '-', '/', '*', '<', '>', '=', '&', '|', '(', ')', ';', ',', '^','[',']'];
 let charactersParenthesisRight = ['/', '*', '(', '['];
 let charactersParenthesisLeft = ['/', '*', ')'];
 
@@ -97,15 +98,15 @@ function substituteLine(statement, vars, should_return_line_on_variable_dec,pars
         substitutedCode += 'if('+substituteExpression(statement.test,vars)+')\n';
         let returnValue = substituteLine(statement.consequent,copyMap(vars),false,parsed_function_params);
         substitutedCode += returnValue.substitutedCode;
-        while(alternate.type != null && alternate.type === 'IfStatement'){
-            substitutedCode += 'else if('+substituteExpression(statement.test,vars)+')\n';
-            returnValue = substituteLine(statement.consequent,copyMap(vars),false,parsed_function_params);
+        while(alternate != null && alternate.type === 'IfStatement'){
+            substitutedCode += 'else if('+substituteExpression(alternate.test,vars)+')\n';
+            returnValue = substituteLine(alternate.consequent,copyMap(vars),false,parsed_function_params);
             substitutedCode += returnValue.substitutedCode;
             alternate = alternate.alternate;
         }
         if(alternate != null){
-            substitutedCode += 'else('+substituteExpression(statement.test,vars)+')\n';
-            returnValue = substituteLine(statement.consequent,copyMap(vars),false,parsed_function_params);
+            substitutedCode += 'else\n';
+            returnValue = substituteLine(alternate,copyMap(vars),false,parsed_function_params);
             substitutedCode += returnValue.substitutedCode;
         }
 
@@ -132,7 +133,9 @@ function substituteLine(statement, vars, should_return_line_on_variable_dec,pars
                         + ' = '+substitutedExpression+';\n';
                 }
             }
-            //TODO: add array support
+            if(statement.expression.left.type === 'MemberExpression'){
+                //TODO: add array support
+            }
         }
         //TODO: check if there are more cases other than 'AssignmentExpression'
     }
@@ -153,6 +156,7 @@ function symbolicSubstitution(parsedCode) {
     let _function = null;
     let substitutedCode_function = '';
     let global_vars = new Map();
+
     Array.prototype.forEach.call(parsedCode.body, (line) => {
         if (line.type === 'VariableDeclaration') {
             if (_function == null) {
@@ -169,6 +173,7 @@ function symbolicSubstitution(parsedCode) {
             _function = line;
         }
     });
+
     globalVars = Object.keys(global_vars);
     let parsedFunctionParams = _function.params.map((param) => {
         return param.name;
@@ -186,19 +191,178 @@ function symbolicSubstitution(parsedCode) {
     return beautify(substitutedCode_before+substitutedCode_function+substitutedCode_after,{indent_size:4});
 }
 
-function colorFunction(functionCode, input) {
-
+function getColoredLines(line,func_and_global_vars) {
+    let coloredLines = [];
+    let done = false;
+    if (line.type === 'IfStatement') {
+        let alternate = line.alternate;
+        let expression_to_eval = escodegen.generate(line.test);
+        expression_to_eval = substituteVars(expression_to_eval,func_and_global_vars);
+        alert(expression_to_eval);
+        let evaluated_expression = eval(expression_to_eval);
+        if(evaluated_expression){
+            done = true;
+        }
+        coloredLines.push({'line':line.loc.start.line,
+            'color':evaluated_expression ? 'green' : 'red'});
+        while(alternate != null && alternate.type === 'IfStatement'){
+            expression_to_eval = escodegen.generate(alternate.test);
+            expression_to_eval = substituteVars(expression_to_eval,func_and_global_vars);
+            if(done){
+                coloredLines.push({'line':alternate.loc.start.line,
+                    'color':'red'});
+            }
+            else{
+                evaluated_expression = eval(expression_to_eval);
+                if(evaluated_expression){
+                    done = true;
+                }
+                coloredLines.push({'line':alternate.loc.start.line,
+                    'color':evaluated_expression ? 'green' : 'red'});
+            }
+            alternate = alternate.alternate;
+        }
+        if(alternate != null){
+            coloredLines.push({'line':alternate.loc.start.line,
+                'color': !done ? 'green' : 'red'});
+        }
+    }
+    else if (line.type === 'WhileStatement') {
+        let expression_to_eval = escodegen.generate(line.test);
+        expression_to_eval = substituteVars(expression_to_eval,func_and_global_vars);
+        coloredLines.push({'line':line.loc.start.line,
+            'color':eval(expression_to_eval) ? 'green' : 'red'});
+    }
+    else if(line.type === 'BlockStatement'){
+        Array.prototype.forEach.call(line.body,(line)=>{
+            coloredLines = coloredLines.concat(getColoredLines(line,func_and_global_vars));
+        });
+    }
+    return coloredLines;
 }
+
+
+function substituteVars(expression,func_and_global_vars) {
+    Array.prototype.forEach.call(Object.keys(func_and_global_vars),(key) => {
+        let index = expression.search(key);
+        if (index >= 0) {
+            let start_left = index, start_right = index + key.length - 1;
+            while (expression[--start_left] === ' ') {/*MAGIC*/}
+            while (expression[++start_right] === ' ') {/*MAGIC*/}
+            let should_substitute = false;
+            let with_parenthesis = false;
+            if (start_left < 0) {
+                if (start_right === expression.length) {
+                    //Substitute and no parenthesis
+                    should_substitute = true;
+                }
+                else if (legalCharacters.includes(expression[start_right])) {
+                    // Substitute
+                    should_substitute = true;
+                    if (charactersParenthesisRight.includes(expression[start_right])) {
+                        //Put parenthesis
+                        with_parenthesis = true;
+                    } else {
+                        // No parenthesis
+                    }
+                }
+            }
+            else {
+                if (start_right === expression.length) {
+                    if (legalCharacters.includes(expression[start_left])) {
+                        // Substitute
+                        should_substitute = true;
+                        if (charactersParenthesisLeft.includes(expression[start_left])) {
+                            //Put parenthesis
+                            with_parenthesis = true;
+                        } else {
+                            // No parenthesis
+                        }
+                    }
+                }
+                else {
+                    if (legalCharacters.includes(expression[start_left]) && legalCharacters.includes(expression[start_right])) {
+                        // Substitute
+                        should_substitute = true;
+                        if (charactersParenthesisLeft.includes(expression[start_left]) || charactersParenthesisRight.includes(expression[start_right])) {
+                            //Put parenthesis
+                            with_parenthesis = true;
+                        } else {
+                            // No parenthesis
+                        }
+                    }
+                }
+            }
+            if (should_substitute) {
+                let new_string = func_and_global_vars[key];
+                if(typeof new_string === 'string' || new_string instanceof String){
+                    new_string = '"'+new_string+'"';
+                }
+                if(new_string.constructor === Array){
+                    new_string = '['+new_string+']';
+                }
+                if (with_parenthesis) {
+                    new_string = '(' + new_string + ')';
+                }
+                expression = expression.substr(0, index) + new_string + expression.substr(index + key.length);
+            }
+        }
+    });
+    return expression;
+}
+
+
+function colorFunction(functionCode, input) {
+    let parsedCode = esprima.parseScript(functionCode,{loc:true,range:true});
+    let _function = null;
+    let global_vars = {};
+    let function_vars = {};
+    Array.prototype.forEach.call(parsedCode.body, (line) => {
+        if(line.type === 'VariableDeclaration'){
+            if (line.declarations[0].type === 'VariableDeclarator') {
+                global_vars[line.declarations[0].id.name] =
+                    eval(escodegen.generate(line.declarations[0].init));
+            }
+        }
+        else if(line.type === 'FunctionDeclaration') {
+            _function = line;
+        }
+    });
+    Array.prototype.forEach.call(_function.params, (param,index) => {
+        function_vars[param.name] = eval(input[index]);
+    });
+    let coloredLines = [];
+    let func_and_global_vars = {};
+    Array.prototype.forEach.call(Object.keys(global_vars),(key)=>{
+        func_and_global_vars[key] = global_vars[key];
+    });
+    Array.prototype.forEach.call(Object.keys(function_vars),(key)=>{
+        func_and_global_vars[key] = function_vars[key];
+    });
+    Array.prototype.forEach.call(_function.body.body, (line) => {
+        coloredLines = coloredLines.concat(getColoredLines(line,
+            func_and_global_vars));
+    });
+    let lines = functionCode.split('\n');
+    Array.prototype.forEach.call(coloredLines,(line)=>{
+        lines[line.line-1] = '<font color="'+line.color+'">'+lines[line.line-1]+'</font>';
+    });
+    lines = lines.map((line)=>{
+        return line + '<br>';
+    });
+
+    return lines.join('\n').replace(/\s\s\s\s/g,'&nbsp;&nbsp;&nbsp;&nbsp;');
+}
+
 
 let globalVars = [];
 let originalCode = '';
 const parseCode = (codeToParse, functionInput) => {
+
     originalCode = codeToParse;
     let parsedCode = esprima.parseScript(codeToParse, {loc: true, range: true});
     let substitutedCode = symbolicSubstitution(parsedCode);
-    let coloredLines = colorFunction(esprima.parseScript(substitutedCode), functionInput);
-    //let returnValue = {code: substitutedCode, colors: coloredLines};
-    return substitutedCode;
+    return colorFunction(substitutedCode, functionInput);
 
 };
 
